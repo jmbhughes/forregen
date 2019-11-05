daae9fdb0309757549b3858f13e9e59d5e742357
Marcus Hughes <hughes.jmb@gmail.com>
2019-11-05 16:27:18 -0700

    up to tutorial

---
+++ src/lSystem/LSystem.java
@@ -0,0 +1,5 @@
+package lSystem;
+
+public class LSystem {
+
+}
--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,42 +1,60 @@
 package renderEngine;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.PixelFormat;
-import org.lwjgl.opengl.ContextAttribs;
 
 
 public class DisplayManager {
-	
+
 	private static final int WIDTH = 1280;
 	private static final int HEIGHT = 720;
 	private static final int FPS_CAP = 120;
 	
-	public static void createDisplay() {
-		ContextAttribs attribs = new ContextAttribs(3,2);
-		attribs.withForwardCompatible(true);
-		attribs.withProfileCore(true);
+	private static final String TITLE = "3D Game Engine - ThinMatrix tutorial";
+
+	private static long lastFrameTime;
+	private static float delta;
+	
+	public static void createDisplay(){
 		
-		// Attempt to create a window
-		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+		ContextAttribs attribs = new ContextAttribs(3,2) /* version of opengl to use */
+		.withForwardCompatible(true)
+		.withProfileCore(true);
+		
+		try {	
+			Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
 			Display.create(new PixelFormat(), attribs);
-		} catch (LWJGLException e) {
+			Display.setTitle(TITLE);
+		} catch (LWJGLException e){
 			e.printStackTrace();
 		}
 		
-		// Use the full window as the viewport
 		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		lastFrameTime = getCurrentTime();
 	}
-	
-	public static void updateDisplay() {
-		Display.sync(FPS_CAP);
+		
+	public static void updateDisplay(){
+		Display.sync( FPS_CAP );
 		Display.update();
+		long currentFrameTime = getCurrentTime();
+		delta = (currentFrameTime - lastFrameTime)/1000f;
+		lastFrameTime = currentFrameTime;
 	}
 	
-	public static void closeDisplay() {
+	public static float getFrameTimeSeconds(){
+		return delta;
+	}
+	
+	public static void closeDisplay(){
 		Display.destroy();
+	}
+	
+	private static long getCurrentTime() {
+		return Sys.getTime()*1000/Sys.getTimerResolution();
 	}
 }
